a7824f60b44898ad7cc0a9deb64052646c8fe6ff
Nonhoman <105434437+Nonhoman@users.noreply.github.com>
2022-05-22 13:33:01 +0800

    config.js

--- config.js
+++ config.js
@@ -37,11 +37,11 @@ global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
 
 //========Url Template Buttons=========//
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/aguzfamilia"
+global.urlnya = "gak ada"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 812-3877-2258'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
